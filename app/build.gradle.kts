03bc99a9f69d9ca5b63186216060f12838174f1e
Adrian <awinf204@gmail.com>
2022-04-11 11:53:33 +0200

    Add domain, data and usecases modules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,7 @@ dependencies {
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.3")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+    implementation(project(":data"))
+    implementation(project(":domain"))
+    implementation(project(":usecases"))
 }

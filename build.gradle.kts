a0456b5512cb1c2a15af77bac4e84953b7897092
Adrian <awinf204@gmail.com>
2022-04-27 12:33:33 +0200

    Add refreshVersions plugin and separate detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,44 +1,23 @@
 import io.gitlab.arturbosch.detekt.Detekt
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "7.1.2" apply false
-    id("com.android.library") version "7.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.6.20" apply false
-    id("io.gitlab.arturbosch.detekt") version "1.19.0"
-}
 
-subprojects {
-
-    apply(plugin = "io.gitlab.arturbosch.detekt")
-
-    detekt {
-        config = files("$rootDir/config/detekt/detekt.yml")
-        buildUponDefaultConfig = true
+@Suppress("GradlePluginVersion")
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
     }
-
-    tasks.withType<Detekt>().configureEach {
-        jvmTarget = "1.8"
-        reports {
-            xml.required.set(true)
-            html.required.set(false)
-            txt.required.set(false)
-            sarif.required.set(false)
-        }
-    }
-
-    afterEvaluate {
-        // we don't want to run the detekt task in every build task to don't impact in
-        // the building time. (check dependsOn build)
-        tasks.named("check").configure {
-            this.setDependsOn(this.dependsOn.filterNot {
-                it is TaskProvider<*> && it.name == "detekt"
-            })
-        }
+    dependencies {
+        classpath("com.android.tools.build:gradle:_")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:_")
+        classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:_")
     }
 }
 
-tasks.register("clean", Delete::class) {
+apply(from = "gradle-scripts/detekt.gradle")
+
+tasks.create("clean", Delete::class) {
     delete(rootProject.buildDir)
 }

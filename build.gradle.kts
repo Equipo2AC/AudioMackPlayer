cfa8b6d2ba7fdf30b36dfd42d1a185a239d83b63
Daniel García Maestro <danielgarciamaestro87@gmail.com>
2022-05-08 09:51:29 +0200

    Fix issues. TODO replace comments for secrets and hardcodes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:_")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:_")
         classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:_")
-        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:_")
+//        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:_")
         classpath("com.google.dagger:hilt-android-gradle-plugin:_")
     }
 }

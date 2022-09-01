fb9fee4aa69a05c28df4204dfbfbfb0ecbc90505
LuisMackenzie <principemackenzie@gmail.com>
2022-09-01 21:45:07 +0200

    fixing use cases

--- usecases/src/main/java/com/ac/musicac/usecases/RequestSeveralArtistUseCase.kt
+++ usecases/src/main/java/com/ac/musicac/usecases/RequestSeveralArtistUseCase.kt
@@ -1,9 +1,10 @@
 package com.ac.musicac.usecases
 
 import com.ac.musicac.data.repository.MusicRepository
+import com.ac.musicac.domain.Error
 import javax.inject.Inject
 
 class RequestSeveralArtistUseCase @Inject constructor(private val repository: MusicRepository) {
 
-    suspend operator fun invoke(ids: String) = repository.getSeveralAlbums(ids)
+    suspend operator fun invoke(ids: String): Error? = repository.getSeveralArtist(ids)
 }

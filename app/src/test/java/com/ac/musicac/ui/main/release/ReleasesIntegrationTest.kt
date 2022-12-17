1bf21eb1a4d78cf494555a7911648c2ae6941d46
Daniel García Maestro <danielgarciamaestro87@gmail.com>
2022-12-17 14:26:19 +0100

    Create releases ViewModel tests.

---
+++ app/src/test/java/com/ac/musicac/ui/main/release/ReleasesIntegrationTest.kt
@@ -0,0 +1,18 @@
+package com.ac.musicac.ui.main.release
+
+import CoroutinesTestRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class ReleasesIntegrationTest {
+
+    @get:Rule
+    val coroutinesTestRule = CoroutinesTestRule()
+
+
+
+}
